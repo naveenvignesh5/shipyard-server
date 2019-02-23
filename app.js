53e2886664e7cfd07f097847f8ad5b9d01c52b6f
naveenvignesh <naveenvignesh5.nv@gmail.com>
2019-02-23 14:52:27 +0530

    Adds test route

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var testRouter = require("./routes/test");
 
 var app = express();
 
@@ -24,8 +24,9 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/", express.static("public"));
 app.use("/users", usersRouter);
+app.use("/tests", testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
